020443ed892b6109d9408b73cef6ec3cc2704015
danny <gryb809@gmail.com>
2021-02-22 22:20:53 +0900

    :sparkles: 기본 기능 테스트용 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import Potato from './Potato';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>"Hello"</h1>
+      <Movie fav="Fight Club"/>
     </div>
   );
+}
+
+// 순서가 아래여도 가능한 듯
+function Movie({fav}) { // 구조 분해 할당
+  return <h1>I like {fav}</h1>
 }
 
 export default App;
---
+++ src/Potato.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Potato() {
+    return <h3>I love potayo</h3>;
+}
+
+export default Potato;
