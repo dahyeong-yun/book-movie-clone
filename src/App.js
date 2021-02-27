ffac2eeb9f1b1d845206d9ac52edfdfe95a10285
danny <gryb809@gmail.com>
2021-02-27 13:56:26 +0900

    :lipstick: CSS 적용

--- src/App.js
+++ src/App.js
@@ -25,21 +25,22 @@ class App extends React.Component {
   render() {
     const {isLoading, movies} = this.state;
     return (
-      <section class="container">
+      <section className="container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader_text">'Loading...'</span>
+          <div className="loader">
+            <span className="loader_text">'Loading...'</span>
           </div> 
           ) : (
-          <div class="movies"> 
-            {movies.map((moive) => (
+          <div className="movies"> 
+            {movies.map((movie) => (
               <Movie
-                key={moive.id}
-                id={moive.id}
-                year={moive.year}
-                title={moive.title}
-                summary={moive.summary}
-                poster={moive.medium_cover_image}
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
           </div>
--- src/Movie.js
+++ src/Movie.js
@@ -2,25 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css'
 
-function Moive({ year, title, summary, poster }) {
+function Movie({ year, title, summary, poster, genres }) {
     return (
-      <div class="movie">  
+      <div className="movie">  
         <img src={poster} alt={title} title={title}/>
-        <div class="movie_data">
-            <h3 class="movie_title">{title}</h3>
-            <h5 class="movie_year">{year}</h5>
-            <p class="movie_summary">{summary}</p>
+        <div className="movie_data">
+            <h3 className="movie_title">{title}</h3>
+            <h5 className="movie_year">{year}</h5>
+            <p className="movie_summary">{summary.slice(0, 180)}...</p>
+            <ul className="movie_genres">              
+              {genres.map((genre, idx) => {
+                return <li key={idx} className="movie_genre">{genre}</li>;
+              })}
+            </ul>
         </div>
       </div>
       
     );   
 }
 
-Moive.propTypes = { 
+Movie.propTypes = { 
     year : PropTypes.number.isRequired,
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
     poster : PropTypes.string.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export default Moive;
+export default Movie;
